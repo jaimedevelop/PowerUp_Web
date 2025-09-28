483f55c156909f4999f73baf5d139db349b0e2c9
Maximilian Fido <maxiu.fido@gmail.com>
2025-09-28 19:42:20 -0400

    test push

--- src/features/athlete/components/train/TrainingCalendar/TrainingCalendar.tsx
+++ src/features/athlete/components/train/TrainingCalendar/TrainingCalendar.tsx
@@ -329,6 +329,7 @@ export const TrainingCalendar: React.FC = () => {
                   }}
                 />
               )}
+              {/*jello*/}
 
               {/* coach notes */}
               {openEvent.ev.coachNotes && (
